b31bc6c78ad59b5614dae4a67eabe235e75724b1
PaulAntonucci <Paul@AlbertisWindow.com>
2015-08-26 11:22:06 -0400

    This must count as a message

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "mainwindow.h"
 #include <QApplication>
-
+gitgit vim
 
 //#include "read_write_globals.h"
 // Now making a change from github.com Aug 25 2015
-
+//   here
 // another change - on the 26th of August
+// well, actually that was the 25th . . .
+// inset mode on VIM,, i or I or a or A, on vi it's |
+// stuff
 
 int main(int argc, char *argv[])
 {
@@ -13,6 +16,21 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    
+   
     return a.exec();
 }
+
+
+
+
+//some sentences - trying out the vi editor
+// what's the difference between vi and vim?
+// alkwaejf
+//
+
+
+
+// what's next?
+// how do I get into/out of insert mode with VIM?
+
+
